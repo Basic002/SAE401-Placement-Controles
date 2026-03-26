671c5747db882d51fc597c66d66ad1623ffdfd52
zedeska <zedeska@protonmail.ch>
2026-03-27 00:57:45 +0100

    affichage gest salle

--- Placement/views/salle/gest_salle.php
+++ Placement/views/salle/gest_salle.php
@@ -8,33 +8,49 @@
 <div class="contenu">
 
     <!-- BOUTONS D'ACTION -->
-    <div class="actions">
-        <a href="index.php?action=crea_salle" class="btn-action">Créer une salle</a>
-        <a href="index.php?action=modif_salle" class="btn-action">Modifier une salle</a>
+    <div class="actions-header">
+        <a href="index.php?action=crea_salle" class="btn-action btn-primary">+ Créer une salle</a>
+        <a href="index.php?action=modif_salle" class="btn-action btn-secondary">✎ Modifier une salle</a>
     </div>
 
-    <!-- ENTETE TABLEAU -->
-    <div class="ligne-entete">
-        <div class="col-nom">Nom</div>
-        <div class="col-bat">Bâtiment</div>
-        <div class="col-etage">Étage</div>
-        <div class="col-cap">Capacité</div>
-        <div class="col-actions">Actions</div>
+    <!-- TABLEAU DES SALLES -->
+    <?php if (!empty($salles)): ?>
+    <table class="tableau-salles">
+        <thead>
+            <tr>
+                <th class="col-nom">Nom</th>
+                <th class="col-bat">Bâtiment</th>
+                <th class="col-etage">Étage</th>
+                <th class="col-dpt">Département</th>
+                <th class="col-cap">Capacité</th>
+                <th class="col-actions">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($salles as $salle): ?>
+            <tr>
+                <td class="col-nom">
+                    <strong><?php echo htmlspecialchars($salle['nom_salle']); ?></strong>
+                </td>
+                <td class="col-bat"><?php echo htmlspecialchars($salle['nom_bat']); ?></td>
+                <td class="col-etage"><?php echo htmlspecialchars($salle['etage']); ?></td>
+                <td class="col-dpt"><?php echo htmlspecialchars($salle['nom_dpt'] ?? 'N/A'); ?></td>
+                <td class="col-cap"><?php echo htmlspecialchars($salle['capacite']); ?> places</td>
+                <td class="col-actions">
+                    <a href="index.php?action=visu_salle&amp;id_salle=<?php echo (int)$salle['id_salle']; ?>" 
+                       class="btn-view" title="Visualiser">
+                        👁️
+                    </a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <?php else: ?>
+    <div class="no-data">
+        <p>Aucune salle créée pour le moment.</p>
+        <a href="index.php?action=crea_salle" class="btn-action btn-primary">Créer la première salle</a>
     </div>
-
-    <!-- LISTE DES SALLES -->
-    <?php foreach ($salles as $salle): ?>
-    <div class="ligne-salle">
-        <div class="col-nom"><?php echo htmlspecialchars($salle['nom_salle']); ?></div>
-        <div class="col-bat"><?php echo htmlspecialchars($salle['nom_bat']); ?></div>
-        <div class="col-etage"><?php echo htmlspecialchars($salle['etage']); ?></div>
-        <div class="col-cap"><?php echo htmlspecialchars($salle['capacite']); ?></div>
-        <div class="col-actions">
-            <a href="index.php?action=visu_salle&amp;id_salle=<?php echo (int)$salle['id_salle']; ?>">
-                <img src="public/images/loupe.png" alt="Visualiser" class="img-action">
-            </a>
-        </div>
-    </div>
-    <?php endforeach; ?>
+    <?php endif; ?>
 
 </div>
