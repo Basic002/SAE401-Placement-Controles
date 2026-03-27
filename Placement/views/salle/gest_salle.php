3e7a1834e638a66e945a8fd1f5949d77ec316b1e
zedeska <zedeska@protonmail.ch>
2026-03-27 01:09:56 +0100

    css fix

--- Placement/views/salle/gest_salle.php
+++ Placement/views/salle/gest_salle.php
@@ -7,49 +7,53 @@
 <!-- ##################### CONTENU PAGE ##################### -->
 <div class="contenu">
 
-    <!-- BOUTONS D'ACTION -->
-    <div class="actions-header">
-        <a href="index.php?action=crea_salle" class="btn-action btn-primary">+ Créer une salle</a>
-        <a href="index.php?action=modif_salle" class="btn-action btn-secondary">✎ Modifier une salle</a>
+    <!-- BOUTON CREATION SALLE -->
+    <div id="btncrea">Créer une salle</div>
+
+    <!-- BLOC CREATION SALLE -->
+    <div id="bloccreasal" style="display:none">
+        <a href="index.php?action=crea_salle">
+            <button type="button">Créer une salle</button>
+        </a>
+        <button type="button" onclick="document.getElementById('bloccreasal').style.display='none';">Annuler</button>
     </div>
 
-    <!-- TABLEAU DES SALLES -->
+    <!-- BOUTON MODIFICATION SALLE -->
+    <div id="btnmodif">Modifier une salle</div>
+
+    <!-- BLOC AFFICHAGE TITRE TABLEAU -->
     <?php if (!empty($salles)): ?>
-    <table class="tableau-salles">
-        <thead>
-            <tr>
-                <th class="col-nom">Nom</th>
-                <th class="col-bat">Bâtiment</th>
-                <th class="col-etage">Étage</th>
-                <th class="col-dpt">Département</th>
-                <th class="col-cap">Capacité</th>
-                <th class="col-actions">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($salles as $salle): ?>
-            <tr>
-                <td class="col-nom">
-                    <strong><?php echo htmlspecialchars($salle['nom_salle']); ?></strong>
-                </td>
-                <td class="col-bat"><?php echo htmlspecialchars($salle['nom_bat']); ?></td>
-                <td class="col-etage"><?php echo htmlspecialchars($salle['etage']); ?></td>
-                <td class="col-dpt"><?php echo htmlspecialchars($salle['nom_dpt'] ?? 'N/A'); ?></td>
-                <td class="col-cap"><?php echo htmlspecialchars($salle['capacite']); ?> places</td>
-                <td class="col-actions">
-                    <a href="index.php?action=visu_salle&amp;id_salle=<?php echo (int)$salle['id_salle']; ?>" 
-                       class="btn-view" title="Visualiser">
-                        👁️
-                    </a>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    <?php else: ?>
-    <div class="no-data">
+    <div id="bloctitre">
+        <div class="nomtitre">Nom</div>
+        <div class="nomtitre">Bâtiment</div>
+        <div class="nomtitre">Étage</div>
+        <div class="nomtitre">Département</div>
+        <div class="nomtitre">Capacité</div>
+        <div class="nomtitre">Actions</div>
+    </div>
+    <?php endif; ?>
+
+    <!-- BLOC AFFICHAGE TABLEAU -->
+    <?php foreach ($salles as $salle): ?>
+    <div class="contenutab">
+        <div class="nomtitre"><?php echo htmlspecialchars($salle['nom_salle']); ?></div>
+        <div class="nomtitre"><?php echo htmlspecialchars($salle['nom_bat']); ?></div>
+        <div class="nomtitre"><?php echo htmlspecialchars($salle['etage']); ?></div>
+        <div class="nomtitre"><?php echo htmlspecialchars($salle['nom_dpt'] ?? 'N/A'); ?></div>
+        <div class="nomtitre"><?php echo htmlspecialchars($salle['capacite']); ?></div>
+        <div class="nomtitre">
+            <a href="index.php?action=visu_salle&amp;id_salle=<?php echo (int)$salle['id_salle']; ?>">
+                <img src="public/images/loupe.png" alt="Visualiser" class="img-action">
+            </a>
+        </div>
+    </div>
+    <?php endforeach; ?>
+
+    <!-- MESSAGE AUCUNE SALLE -->
+    <?php if (empty($salles)): ?>
+    <div id="nodata">
         <p>Aucune salle créée pour le moment.</p>
-        <a href="index.php?action=crea_salle" class="btn-action btn-primary">Créer la première salle</a>
+        <a href="index.php?action=crea_salle"><button>Créer une salle</button></a>
     </div>
     <?php endif; ?>
 
